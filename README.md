e9ad64e85b1f98929309cb4ddfa321d7faea1638
Martin Rohart <martin.rohart@kindredgroup.com>
2018-12-03 15:59:43 +0100

    day 3 small code prettyfyings

---
+++ README.md
@@ -0,0 +1,4 @@
+# AdventOfCode2018
+
+It's this time of the year again!
+https://adventofcode.com/2018
--- day3.py
+++ day3.py
@@ -6,30 +6,27 @@ with open ("input3.txt", "r") as file:
 pattern = re.compile(r'#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)')
 
 
-rectangles = list(map(lambda s: pattern.match(s),strings))
+rectangles = map(lambda s: pattern.match(s).groups(),strings)
 
-fabric = [[0 for j in range(1000)] for i in range(1000)]
-shared=0
+fabric = [[0 for j in range(1500)] for i in range(1500)]
+shared = 0
 
 for rectangle in rectangles:
-	for i in range(int(rectangle.group(2)), int(rectangle.group(2))+int(rectangle.group(4))):
-		for j in range(int(rectangle.group(3)), int(rectangle.group(3))+int(rectangle.group(5))):
+	for i in range(int(rectangle[1]), int(rectangle[1])+int(rectangle[3])):
+		for j in range(int(rectangle[2]), int(rectangle[2])+int(rectangle[4])):
 			if(fabric[i][j]!=0 and fabric[i][j]!='x'):
 				fabric[i][j]='x'
 				shared+=1
-			elif fabric[i][j]=='x':
-				#nothing
-				fabric[i][j]='x'
-			else:
-				fabric[i][j]=rectangle.group(1)
+			elif fabric[i][j]!='x':
+				fabric[i][j]=rectangle[0]
 print shared
 
 #Part 2
 def checkUnique(rectangle):
-	for i in range(int(rectangle.group(2)), int(rectangle.group(2))+int(rectangle.group(4))):
-		for j in range(int(rectangle.group(3)), int(rectangle.group(3))+int(rectangle.group(5))):
-			if fabric[i][j]!=rectangle.group(1):
+	for i in range(int(rectangle[1]), int(rectangle[1])+int(rectangle[3])):
+		for j in range(int(rectangle[2]), int(rectangle[2])+int(rectangle[4])):
+			if fabric[i][j]!=rectangle[0]:
 				return False
 	return True
 
-print filter(checkUnique, rectangles)[0].group(1)
+print filter(checkUnique, rectangles)[0][0]
