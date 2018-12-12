09257030578d37e5c16c5402bbbbea61c0dcaca0
Martin Rohart <martin.rohart@kindredgroup.com>
2018-12-12 16:53:25 +0100

    condensing day 1

--- day1.py
+++ day1.py
@@ -1,23 +1,13 @@
-with open ("input1.txt", "r") as myfile:
-	input=myfile.read()
-frequencies = input.split('\n');
-
-final = 0;
-for frequency in frequencies:
-	final = final + int(frequency)
-
-print "Final frequency: "+ str(final)
+frequencies = open ("input1.txt", "r").read().split('\n')
+print sum([int(f) for f in frequencies]);
 
 #Part 2
-
 final = 0;
 keys = {}
 i = 0
-while i<len(frequencies):
-	final = final + int(frequencies[i])
+while True:
+	final = final + int(frequencies[i % len(frequencies)])
 	i+=1
-	if i==len(frequencies):
-		i = 0
 	if str(final) in keys:
 		print "Frequency reached again: "+str(final)
 		break
